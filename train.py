89190d7621904a4bed0d5db39698da683545983d
Chua Jia yang <chuajiayang@192.168.1.92>
2025-04-09 02:34:50 +0800

    Added Initial Implementation of FFAnet, ICB, KJRD and FasterRCNN

---
+++ train.py
@@ -0,0 +1,27 @@
+# python train.py --config_file configs/config_ffa_net.yaml
+
+import argparse
+import yaml
+from trainers.trainer_ffa_net import FFANetTrainer
+from config import Config
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="trainer")
+    parser.add_argument('--config_file', type=str, default='configs/config.yaml', help="path to YAML config")
+    parser.add_argument('--output_dir', type=str, default=None, help="path to output directory (optional); defaults to outputs/model_name")
+    args = parser.parse_args()
+
+    # Load YAML configuration
+    with open(args.config_file, 'r') as file:
+        config_dict = yaml.safe_load(file)
+        config = Config(config_dict=config_dict)
+
+    if config.network.model.lower() == 'ffa_net':
+        net_trainer = FFANetTrainer(config=config, output_dir=args.output_dir)
+
+    else:
+        raise ValueError(f"{config.network.model} not supported.") 
+    
+    net_trainer.train()
