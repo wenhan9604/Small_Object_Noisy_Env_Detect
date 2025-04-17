4a59eb6e82c33279f754aa2fb3404cd3cd117098
Gary <hfoo3@gatech.edu>
2025-04-17 21:04:32 +0800

    intermediate update

--- train.py
+++ train.py
@@ -3,6 +3,7 @@
 import argparse
 import yaml
 from trainers.trainer_ffa_net import FFANetTrainer
+from trainers.trainer_rcan import RCANTrainer
 from config import Config
 
 
@@ -10,7 +11,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="trainer")
     parser.add_argument('--config_file', type=str, default='configs/config.yaml', help="path to YAML config")
-    parser.add_argument('--output_dir', type=str, default=None, help="path to output directory (optional); defaults to outputs/model_name")
+    parser.add_argument('--output_dir', type=str, default='None', help="path to output directory (optional); defaults to outputs/model_name")
     args = parser.parse_args()
 
     # Load YAML configuration
@@ -20,6 +21,8 @@ if __name__ == "__main__":
 
     if config.network.model.lower() == 'ffa_net':
         net_trainer = FFANetTrainer(config=config, output_dir=args.output_dir)
+    elif config.network.model.lower() == 'rcan':
+            net_trainer = RCANTrainer(config=config, output_dir=args.output_dir)
 
     else:
         raise ValueError(f"{config.network.model} not supported.") 
