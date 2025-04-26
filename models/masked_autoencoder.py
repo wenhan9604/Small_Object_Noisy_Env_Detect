cc06aa2f5cbd35114678151dabfc2710e4e5c2ce
Wen Han <wenhan9604@gmail.com>
2025-04-26 14:20:04 +0800

    feat: refactored masked autoencoder into class

--- models/masked_autoencoder.py
+++ models/masked_autoencoder.py
@@ -1,162 +1,64 @@
-import sys
-import os
-import requests
 import torch
+import torch.nn as nn
 import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
 from mae import models_mae
 
-imagenet_mean = np.array([0.485, 0.456, 0.406])
-imagenet_std = np.array([0.229, 0.224, 0.225])
+class MaskedAutoEncoder(nn.Module):
+    def __init__(
+            self,
+            chkpt_dir = './checkpoint/mae_pretrain_vit_large.pth',
+            model_arch = 'mae_vit_large_patch16',
+            imagenet_mean = np.array([0.485, 0.456, 0.406]),
+            imagenet_std = np.array([0.229, 0.224, 0.225])
+            ):
 
-def show_image(image, title=''):
-    # image is [H, W, 3]
-    assert image.shape[2] == 3
-    plt.imshow(torch.clip((image * imagenet_std + imagenet_mean) * 255, 0, 255).int())
-    plt.title(title, fontsize=16)
-    plt.axis('off')
-    return
+        super().__init__()
+        self.model = self.prepare_model(chkpt_dir, model_arch)
+        self.imagenet_mean = imagenet_mean
+        self.imagenet_std = imagenet_std
 
-def prepare_model(chkpt_dir, arch='mae_vit_base_patch16'):
-    # build model
-    model = getattr(models_mae, arch)()
-    # load model
-    checkpoint = torch.load(chkpt_dir, map_location='cpu')
-    msg = model.load_state_dict(checkpoint['model'], strict=False)
-    print(msg)
-    return model
 
-def run_one_image(img, model):
+    def forward(self, img):
 
-    x = torch.tensor(img)
+        processed_img = self.ViT_image_preprocessing(img)
+        embedding = self.image_encoding(processed_img, self.model)
 
-    #make it a batch-like
-    x = x.unsqueeze(dim=0)
-    x = torch.einsum('nhwc->nchw', x)
-
-    # run MAE
-    loss, y, mask, latent_embedding = model(x.float(), mask_ratio=0.75)
-    y = model.unpatchify(y)
-    y = torch.einsum('nchw->nhwc', y).detach().cpu()
-
-    #visualize the mask 
-    mask = mask.detach()
-    mask = mask.unsqueeze(-1).repeat(1, 1, model.patch_embed.patch_size[0]**2 *3)  # (N, H*W, p*p*3)
-    mask = model.unpatchify(mask)  # 1 is removing, 0 is keeping
-    mask = torch.einsum('nchw->nhwc', mask).detach().cpu()
+        return embedding
     
-    x = torch.einsum('nchw->nhwc', x)
+    def prepare_model(self, chkpt_dir, arch='mae_vit_base_patch16'):
 
-    # masked image
-    im_masked = x * (1 - mask)
+        # build model
+        model = getattr(models_mae, arch)()
+        # load model
+        checkpoint = torch.load(chkpt_dir, map_location='cpu')
+        msg = model.load_state_dict(checkpoint['model'], strict=False)
+        print(msg)
+        return model
+    
+    def ViT_image_preprocessing(self, img):
 
-    # MAE reconstruction pasted with visible patches
-    im_paste = x * (1 - mask) + y * mask
+        img = img.resize((224, 224))
+        img = np.array(img) / 255.
 
-    # make the plt figure larger
-    plt.rcParams['figure.figsize'] = [24, 24]
+        assert img.shape == (224, 224, 3)
 
-    plt.subplot(1, 4, 1)
-    show_image(x[0], "original")
+        # normalize by ImageNet mean and std
+        img = img - self.imagenet_mean
+        img = img / self.imagenet_std
 
-    plt.subplot(1, 4, 2)
-    show_image(im_masked[0], "masked")
+    def image_encoding(self, img, model):
 
-    plt.subplot(1, 4, 3)
-    show_image(y[0], "reconstruction")
+        x = torch.tensor(img)
 
-    plt.subplot(1, 4, 4)
-    show_image(im_paste[0], "reconstruction + visible")
+        #make it a batch-like
+        x = x.unsqueeze(dim=0)
+        x = torch.einsum('nhwc->nchw', x)
 
-    plt.show()
+        # run MAE
+        loss, y, mask, latent = model(x.float(), mask_ratio=0.75)
+        # y = model.unpatchify(y)
+        # y = torch.einsum('nchw->nhwc', y).detach().cpu()
 
-def ViT_image_preprocessing(img):
-
-    imagenet_mean = np.array([0.485, 0.456, 0.406])
-    imagenet_std = np.array([0.229, 0.224, 0.225])
-
-    img = img.resize((224, 224))
-    img = np.array(img) / 255.
-
-    assert img.shape == (224, 224, 3)
-
-    # normalize by ImageNet mean and std
-    img = img - imagenet_mean
-    img = img / imagenet_std
-
-def image_encoding(img, model):
-
-    x = torch.tensor(img)
-
-    #make it a batch-like
-    x = x.unsqueeze(dim=0)
-    x = torch.einsum('nhwc->nchw', x)
-
-    # run MAE
-    loss, y, mask, latent = model(x.float(), mask_ratio=0.75)
-    y = model.unpatchify(y)
-    y = torch.einsum('nchw->nhwc', y).detach().cpu()
-
-    return latent
-
-def mae_setup_encoder(chkpt_dir = './checkpoint/mae_pretrain_vit_large.pth', model_name = 'mae_vit_large_patch16'):
-    '''
-    Load encoder using the model's path and name
-    '''
-    chkpt_dir = './checkpoint/mae_pretrain_vit_large.pth'
-    model_name = 'mae_vit_large_patch16'
-    model_mae = prepare_model(chkpt_dir, model_name)
-    print('Masked_Autoencoder loaded')
-
-    return model_mae
-
-def mae_encode_image(img, model):
-
-    processed_img = ViT_image_preprocessing(img)
-
-    #Debug preprocessed img
-    # plt.rcParams['figure.figsize'] = [5, 5]
-    # show_image(torch.tensor(processed_img))
-
-    embedding = image_encoding(processed_img, model)
-
-    #Debug embedding
-    # print(f"Embedding Shape: {embedding.shape}")
-    # print(embedding)
-
-    return embedding
-
-def test():
-
-    #Load model
-    chkpt_dir = './checkpoint/mae_pretrain_vit_large.pth'
-    model_mae = prepare_model(chkpt_dir, 'mae_vit_large_patch16')
-    print('Model loaded.')
-
-    # load an image
-    img_url = 'https://user-images.githubusercontent.com/11435359/147738734-196fd92f-9260-48d5-ba7e-bf103d29364d.jpg' # fox, from ILSVRC2012_val_00046145
-    # img_url = 'https://user-images.githubusercontent.com/11435359/147743081-0428eecf-89e5-4e07-8da5-a30fd73cc0ba.jpg' # cucumber, from ILSVRC2012_val_00047851
-    img = Image.open(requests.get(img_url, stream=True).raw)
-    img = img.resize((224, 224))
-    img = np.array(img) / 255.
-
-    assert img.shape == (224, 224, 3)
-
-    # normalize by ImageNet mean and std
-    img = img - imagenet_mean
-    img = img / imagenet_std
-
-    plt.rcParams['figure.figsize'] = [5, 5]
-    show_image(torch.tensor(img))
-
-    # print('MAE with pixel reconstruction:')
-    # run_one_image(img, model_mae)
-
-    print("Retrieve embedding")
-    embedding = image_encoding(img, model_mae)
-
-    print(f"Embedding Shape: {embedding.shape}")
-    print(embedding)
+        return latent
 
 
